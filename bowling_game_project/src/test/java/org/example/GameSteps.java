0ea0028249d298cab8d67b386ad4673ba85d8f3e
Benjamin S <39574422+pl0xi@users.noreply.github.com>
2023-10-07 21:15:53 +0200

    Test 2 done 2.2

--- bowling_game_project/src/test/java/org/example/GameSteps.java
+++ bowling_game_project/src/test/java/org/example/GameSteps.java
@@ -1,24 +1,47 @@
 package org.example;
+import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 import static org.junit.Assert.assertEquals;
 public class GameSteps {
-    Game g = new Game();
+    Game g;
+
+    @Before
+    public void gameSetup() {
+        g = new Game();
+    }
+
     @Given("I am very bad, and can't hit a single pin doing the game")
     public void i_am_very_bad_and_can_t_hit_a_single_pin_doing_the_game() { }
 
-    @When("I play the {int} round game and i hit {int} pins in each round")
+    @When("I played {int} rounds and i hit {int} pins in each round")
     public void i_play_the_round_game_and_i_hit_pins_in_each_round(int rounds, int pins) {
-        for (int i=0; i<rounds; i++) {
-            g.roll(pins);
-        }
-        assertEquals(0, g.score());
+        rollMany(rounds, pins);
     }
 
-    @Then("the score should be {int}")
+    @Then("the score should be {int} because i didn't hit any pin")
+    public void i_should_have_no_points(int expectedPoints) {
+        assertEquals(expectedPoints, g.score());
+    }
+
+    @Given("I am bad, but i was able to hit 1 pin in each round")
+    public void i_am_bad_but_i_was_able_to_hit_1_pin_in_each_round() { }
+
+    @When("I played {int} rounds and i was able to hit {int} pin in each round")
+    public void i_play_the_round_game_and_i_was_able_to_hit_pin_in_each_round(int rounds, int pins) {
+        rollMany(rounds, pins);
+    }
+
+    @Then("the score should be {int} because i hit 1 pin in each round")
     public void i_should_have_points(int expectedPoints) {
         assertEquals(expectedPoints, g.score());
     }
+
+    private void rollMany(int n, int pins) {
+        for (int i = 0; i < n; i++)
+            g.roll(pins);
+    }
+
 }
--- bowling_game_project/src/test/resources/org/example/BowlingGame.feature
+++ bowling_game_project/src/test/resources/org/example/BowlingGame.feature
@@ -3,5 +3,10 @@ Feature: Bowling game system
 
   Scenario: The bowling player is very bad and he can't hit any pin in the game
     Given I am very bad, and can't hit a single pin doing the game
-    When I play the 20 round game and i hit 0 pins in each round
-    Then the score should be 0
+    When I played 20 rounds and i hit 0 pins in each round
+    Then the score should be 0 because i didn't hit any pin
+
+  Scenario: The bowling player is bad and he can only hit 1 pin in each round
+    Given I am bad, but i was able to hit 1 pin in each round
+    When I played 20 rounds and i was able to hit 1 pin in each round
+    Then the score should be 20 because i hit 1 pin in each round
